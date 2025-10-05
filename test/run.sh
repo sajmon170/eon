733e1072efa8555935502cd6b63bc3ad8c2b823f
sajmon <szymon17099@gmail.com>
2025-10-06 00:53:16 +0200

    Add test runner output mount

--- test/run.sh
+++ test/run.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 docker run \
-	--mount type=bind,source=../target/release/eon-client,target=/usr/local/app/eon-client \
-	--mount type=bind,source=.,target=/usr/local/app/data \
-	sajmon/shadow shadow /usr/local/app/data/scenario/simple/simple.yaml
+	--shm-size=1024g --security-opt seccomp=unconfined \
+	--mount type=bind,ro,source=./output/release,target=/usr/local/app/eon-client \
+	--mount type=bind,source=./output/shadow,target=/usr/local/app/output \
+	--mount type=bind,ro,source=./scenario,target=/usr/local/app/scenario \
+	sajmon/shadow bash -c "shadow /usr/local/app/scenario/simple/simple.yaml && mv shadow.data/* /usr/local/app/output/"
