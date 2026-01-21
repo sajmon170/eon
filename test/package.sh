cfb756eec7930f153d94667fbc7167d4ec46e0b9
sajmon <szymon17099@gmail.com>
2026-01-21 13:31:58 +0100

    Move Shadow testing outside

--- test/package.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-docker run \
-	--shm-size=1024g --security-opt seccomp=unconfined \
-	--mount type=bind,ro,source=../,target=/usr/local/app/build \
-	--mount type=bind,source=./output,target=/usr/local/app/build/target \
-    sajmon/shadow bash -c "cd /usr/local/app/build && cargo build --release"
